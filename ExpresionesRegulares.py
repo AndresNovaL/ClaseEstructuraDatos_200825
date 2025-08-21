2dbe626f24edcfbe4304387643947947ca24ba43
Andres Felipe Nova <afnoval@academia.usbbog.edu.co>
2025-08-21 16:20:22 -0500

    Comando para extraer caracteres especiales

--- ExpresionesRegulares.py
+++ ExpresionesRegulares.py
@@ -1,8 +1,11 @@
-import re
+import re # Librería necesaria para utilizar Regex
 
-regularEx_1 = r"\b\w{5}\b"
+# \w = toma caracteres como letras, números o el guión bajo _
+# \s = espacios en blanco
+# \b = establece un límite de palabra
 
-## La palabra seleccionada tiene 5 caracteres
+regularEx_1 = r"\b\w{5}\b" # Buscar una palabra que contenga la cantidad de caracteres especificada.
+
 palabra_1 = """'Cause I'm on top of the world, 'ey
 I'm on top of the world, 'ey
 Waiting on this for a while now
@@ -11,13 +14,18 @@ I've been waiting to smile, 'ey
 Been holding it in for a while, 'ey
 Take it with me if I can
 Been dreaming of this since a child
-I'm on top of the world"""
+I'm on top of the world
+ADD: Are we in Data Structure class?"""
 
-resultado = re.findall(regularEx_1,palabra_1)
+resultado = re.findall(regularEx_1,palabra_1) # Comando para buscar las coincidencias del patrón especificadp dentro del texto.
 print(resultado)
 
-## La palabra seleccionada es 'child'
-regularEx_2 = r"\bchild\b"
+regularEx_2 = r"\bchild\b" # Comando para extraer una palabra especifica, 'child'
 
 resultado = re.findall(regularEx_2,palabra_1)
+print(resultado)
+
+regularEx_3 = r"\?" # Extraer carácteres especiales
+
+resultado = re.findall(regularEx_3,palabra_1)
 print(resultado)
